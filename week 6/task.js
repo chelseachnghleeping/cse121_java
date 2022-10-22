b2e58d44751db6f546d552fd6587eb4cc05085b6
chelseachnghleeping <chn22001@byui.edu>
2022-10-23 00:37:59 +0800

    update files

--- week 6/task.js
+++ week 6/task.js
@@ -1,55 +1,116 @@
-const data = [
+/// HEADING /// 
+const today = new Date();
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+};
+
+document.getElementById("heading").innerHTML = today.toLocaleDateString(
+    "en-US",
+    options
+);
+
+/// DATA ///
+var d = new Date();
+var currentMonthDate =
+    (d.getMonth() + 1 < 10 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1) + 
+    "/" + 
+    d.getDate();
+
+/// DATE : MM/DD/YYYY
+const data = [ 
     {
-        id: 1
+        id: 1,
         name: 'Chelsea Chngh',
-        age: 29,
+        date: 10/23/1993,
         image: 'img/rooster.jpg'
     },
     {
         id: 2,
         name: 'Chester Chngh',
-        age: 27,
+        date: currentMonthDate + "/1997", 
         image: 'img/pig_image.png'
     },
     {
         id: 3,
         name: 'Darren Chngh',
+        date: currentMonthDate + "/1996",
         image: 'img/mouse.jpeg'
     },
-]
+    {
+        id: 4,
+        name: 'Carolina A. Chngh',
+        date: 12/22/1965,
+        image: 'img/snake.jpeg'
+    },
+];
 
-function List({people}) {
-    return (
-        <>
-        {people.map(person => {
-            const {id, name, age, image} = person 
-            return <artcicle key={id} className="person">
-                <img src={image} alt={name} />
-                <div>
-                    <h4>{name}</h4>
-                    <p>{age} years</p>
-                </div>
-                </artcicle>
-            })}
-        </>
-    )
-}
+/// TODAY BIRTHDAY DATA FILTER ///
+var todayData = [];
 
-function App() {
-    const [people, setPeople] = React.useState(data)
+const currentDate = new Date().getDate();
+const currentMonth = new Date().getMonth();
 
+todayData = data,filter((person) => {
+    const {date} = person;
     return(
-        <main>
-            <section className="container">
-                <h2 className="title">{people.length} birthday today</h2>
-                <List people={people} />
+        parseInt(data.substring(3, 5)) === currentDate &&
+        parseInt(date.substring(0, 2)) === currentMonth + 1
+    );
+});
 
-                <button onClick={() => setPeople([])}>
-                    Clear All
-                </button>
-            </section>
-        </main>
-    )
+/// DISPLAY ///
+
+var noOfBirthdays = document.getElementById("noOfBirthdays");
+noOfBirthdays.innerHTML = todayData.length + " birthdays today";
+
+var people = document.getElementById("people");
+
+todayData.forEach((person) => {
+    const { name, date, image } = person;
+
+    var article = document.createElement("article");
+    article.setAttribute("class", "person");
+
+    var img = new Image();
+    img.src = image;
+    img.setAttribute("alt", name);
+
+    var info = document.createElement("div");
+
+    var h4 = document.createElement("h4");
+    h4.setAttribute("id", "name");
+    h4.innerHTML = name;
+
+    var p = document.createElement("p");
+    var dob = new Date(date);
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
+
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff);
+
+    //extract year from date
+    var year = age_dt.getUTCFullYear();
+
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
+    p.innerHTML = age + "years";
+
+    info.appendChild(h4);
+    info.appendChild(p);
+
+    article.appendChild(img);
+    article.appendChild(info);
+
+    people.appendChild(article);
+});
+
+/// clear ///
+
+function clearAll() {
+    people.innerHTML = "";
+    noOfBirthdays.innerHTML = "0 birthdays today";
 }
 
-ReactDOM.render(<App/>, document,querySelector('#root'))
